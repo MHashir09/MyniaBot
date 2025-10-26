704edf105c989f0d2f28adb78d2d8eecb357d2a0
MHashir09 <hashirzubair5742@gmail.com>
2025-10-26 18:46:43 +0500

    feat: add an intent to know about guild members

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
-// Require the necessary discord.js classes
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, MessageFlags, Events, GatewayIntentBits } = require('discord.js');
+// >>> This part requires discord.js classes and modules <<<
+const fs = require('node:fs'); // this module reads files and do similar stuff like this
+const path = require('node:path'); // this module builds path to folders and files etc
+const { Client, Collection, GatewayIntentBits } = require('discord.js'); // requires important classes
 const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// >>> This part creates a new client instance <<<
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
-// When the client is ready, run this code (only once).
-client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-
-    readyClient.user.setPresence({
-        status: 'online',
-    });
-    console.log(`Presence status set to: Online`);
-});
-
-// Log in to Discord with your client's token
-client.login(token);
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+// >>> This part handles commands <<<
+client.commands = new Collection(); // makes a collection to store all the commands by keys(names) and values
+const foldersPath = path.join(__dirname, 'commands'); // builds the path to this folder
+const commandFolders = fs.readdirSync(foldersPath); // returns the folder/files in this path
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	const commandsPath = path.join(foldersPath, folder); // builds the path to that folder
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js')); // returns an array of all the files inside that folder with suffix ".js"
 
 	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		const filePath = path.join(commandsPath, file); // builds the path for that file
+		const command = require(filePath); // loads the command file at that path for to use below
+		// Set a new item in the Collection as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -40,29 +28,19 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
+// >>> This part handle events <<<
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args)); // (...args) is simply whatever argument discord sends and ... unpacks those values and passes them as separate arguments to the execute function
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
+}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			});
-		} else {
-			await interaction.reply({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			});
-		}
-	}
-});
+// >>> This part logs in the discord bot using the token<<<
+client.login(token);
